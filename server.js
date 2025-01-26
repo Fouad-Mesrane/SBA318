083eb917011d41dab63c2ff00d116d5d1a8794df
fouad mesrane <fouadmesrane@gmail.com>
2025-01-25 17:35:09 -0700

    setup the server and installed packages needed

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server Running at PORT :`, PORT)
+})
+
+
+
