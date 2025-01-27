eecdcba83b843e1cdac7d8a1de3e6c7f30e4703d
fouad mesrane <fouadmesrane@gmail.com>
2025-01-26 19:39:17 -0700

    added put and delete methods for orders

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -18,6 +18,30 @@ router
     }
   );
 
-  
+// update orders
+router
+  .route(
+    "/:id",
+    validateRequest(["userId", "productId", "quantity", "status"]),
+    (req, res) => {
+      const order = orders.find((o) => o.id === +req.params.id);
+      if (order) {
+        Object.assign(order, req.body);
+        res.json(order);
+      } else {
+        res.status(404).send("Order not found");
+      }
+    }
+  )
+  // delete order
+  .delete((req, res) => {
+    const index = orders.findIndex((o) => o.id === parseInt(req.params.id));
+    if (index !== -1) {
+      orders.splice(index, 1);
+      res.sendStatus(204);
+    } else {
+      res.status(404).send("Order not found");
+    }
+  });
 
 export default router;
